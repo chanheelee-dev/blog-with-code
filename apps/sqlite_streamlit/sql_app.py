3a52a7c4289a1443183829370706bb607ba03dc8
Chanhee Lee <qjzx6428@gmail.com>
2024-08-13 22:02:22 +0900

    SQL 쿼리 실행 및 조회 다운로드 앱 작성

--- apps/sqlite_streamlit/sql_app.py
+++ apps/sqlite_streamlit/sql_app.py
@@ -5,19 +5,43 @@ import db_util as db
 
 
 st.title("SQL Demo")
+################
+
+st.subheader("Database and Tables")
 
 # show tables
 db_name = st.selectbox(label="Select a database", options=db.get_database_names())
-st.write("Tables in the selected database:")
-st.table(db.get_table_names(db_name))
+tbl_names = db.get_table_names(db_name)
+df_tbl = pd.DataFrame(tbl_names, columns=["Table Name"])
+st.write("", df_tbl)
+
+################
+st.divider()
+################
+
+st.subheader("Query and Result")
 
 # query editor
-query = st.text_area(label="Enter your SQL query here", value="SELECT * FROM data")
+default_table_name = tbl_names[0]
+query = st.text_area(
+    label="Enter your SQL query here",
+    value=f"SELECT * FROM {default_table_name} LIMIT 10;",
+)
 
-# show result
-if st.button("Run query"):
+# run query and show result
+def run_query(db_name, query):
     result = db.execute_query(db_name, query)
-    st.text("Query result:")
-    st.write(pd.DataFrame(result))
-else:
-    st.text("Query result:")
+    return pd.DataFrame(result)
+
+
+run_button = st.button("Run query")
+st.text("Query result:")
+if run_button:
+    df_result = run_query(db_name, query)
+    st.write(df_result)
+    st.download_button(
+        label="Download CSV",
+        data=df_result.to_csv(index=False),
+        file_name="query_result.csv",
+        mime="text/csv",
+    )
